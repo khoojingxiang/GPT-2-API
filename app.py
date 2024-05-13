1c47619bbee788f798d8430bd944503115fdee8e
Khoo Jing Xiang <122330938+khoojingxiang@users.noreply.github.com>
2024-05-13 15:25:56 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, request, jsonify
+import requests
+app = Flask(__name__)
+
+API_URL = "https://api-inference.huggingface.co/models/openai-community/gpt2"
+headers = {"Authorization": "Bearer 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"}
+
+@app.route("/generate-text",methods=['POST'])
+#method
+def generate_text():
+    data = request.get_json() #request POST data
+    #input validation handling
+    if not data or not data.get('inputs'):
+        return jsonify({'Error': 'Missing "inputs" fields in request data'}), 400
+    #return response from gpt-2
+    response = requests.post(API_URL, headers=headers, json=data)
+    return response.json()
+
+@app.route('/')
+def index():
+    # code to handle the root path (e.g., return a welcome message)
+    instructions = (
+        "Welcome to Jing Xiang API!\n\n"
+
+        "For this API to work, you'll need the Postman application.\n\n"
+
+        "To use the GPT-2 API:\n\n"
+
+        "1. In the Postman web application (or download it), set the request URL to:\n"
+        "```\n"
+        "[https://rhino-aware-unduly.ngrok-free.app/generate-text](https://rhino-aware-unduly.ngrok-free.app/generate-text)\n"
+        "```\n\n"
+
+        "2. Set the request method to \"POST\".\n\n"
+
+        "3. Go to the request body and choose \"json\" as the type.\n\n"
+
+        "4. Use the following JSON format:\n"
+        "```json\n"
+        "{\"inputs\": \"*Type your sentence here*\"}"
+        "```\n"
+        "Replace \"*Type your sentence here*\" with the sentence you want the model to continue.\n\n"
+
+        "5. In the response body, you should see your sentence elaborated upon.\n"
+    )
+
+    return instructions
+    
+# Run the API using flask
+if __name__ == "__main__":
+    app.run(debug=True)
+    
